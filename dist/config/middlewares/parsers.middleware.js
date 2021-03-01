999ebc87184103a1385b07396a1168a2ef50a249
dogancancelik12 <dogancancelik@sabanciuniv.edu>
2021-03-01 17:41:57 +0300

    fixes

--- dist/config/middlewares/parsers.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bodyParser = require("body-parser");
-var config = function (app) {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-};
-exports.default = { config: config };
-//# sourceMappingURL=parsers.middleware.js.map
